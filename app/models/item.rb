68e6e42ff33c36dbb15cc826fcda8debf4f19e04
Dante Carlesimo <dantecarlesimo@gmail.com>
2014-05-25 19:01:36 -0700

    added validations and associations to Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ActiveRecord::Base
+
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :sale_id, presence: true
+ 
   belongs_to :sale
+  has_one :transaction
+  
 end
