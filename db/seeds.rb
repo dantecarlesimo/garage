58881e1a4a82489fc828cc0d9a7bbf75223e5350
Dante Carlesimo <dantecarlesimo@gmail.com>
2014-05-25 19:17:53 -0700

    added seed data for sales

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@ User.create([
     {name: "Liam Brown", email: "liam@brown.com", password: "1234567890", password_confirmation: "1234567890", phone: "6502530000"},
     {name: "Olivia Jones", email: "olivia@jones.com", password: "1234567890", password_confirmation: "1234567890"},
   ])
+
+Sale.create([
+    {user_id: 1, title: "Moving Sale", address: "123 Main St.", city: "Alameda", zipcode: 94501, date:"June 1, 2014", time_start: "7 am", time_end: "2 pm", description: "Moving out of state. Tons of great furniture!"},
+    {user_id: 2, title: "Estate Sale", address: "456 State St.", city: "Daly City", zipcode: 94014, date:"June 15, 2014", time_start: "8 am", time_end: "4 pm", description: "Everything must go! Cheap prices"},
+    {user_id: 3, title: "Antiques", address: "789 Church St.", city: "San Francisco", zipcode: 94114, date:"June 9, 2014", time_start: "7 am", time_end: "6 pm", description: "Great antiques and collectables"},
+    {user_id: 4, title: "Kids Toy Sale", address: "1234 Side St.", city: "San Mateo", zipcode: 94002, date:"June 23, 2014", time_start: "7 am", time_end: "1 pm", description: "Lots of kids toys, stuffed animals, books, etc"},
+    {user_id: 5, title: "Kitchen Sale", address: "5678 Baker St.", city: "San Francisco", zipcode: 94114, date:"June 11, 2014", time_start: "6 am", time_end: "1 pm", description: "Kitchen appliances, cookware, and dishes"}
+  ])
