35b8ffcfbc5bccfb981dc99c5bc3b8cffb4e916d
Sterling Radcliff <radcliff@riseup.net>
2014-05-25 18:30:13 -0700

    added rspec tests to user model; added user seed data

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+User.create([
+    {name: "John Smith", email: "john@smith.com", password: "1234567890", password_confirmation: "1234567890", phone: "3479214269"},
+    {name: "Noah Johnson", email: "noah@johnson.com", password: "1234567890", password_confirmation: "1234567890", phone: "4152229670"},
+    {name: "Sophia Williams", email: "sophia@williams.com", password: "1234567890", password_confirmation: "1234567890", phone: "6505434800"},
+    {name: "Liam Brown", email: "liam@brown.com", password: "1234567890", password_confirmation: "1234567890", phone: "6502530000"},
+    {name: "Olivia Jones", email: "olivia@jones.com", password: "1234567890", password_confirmation: "1234567890"},
+  ])
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,38 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do #instantiates a new user before each test
+    @user = User.create(name: "John Smith", email: "john@smith.com", password: "1234567890", password_confirmation: "1234567890")
+  end
+
+  it 'should be able to be created' do #test that model/table set up correctly
+    @user.should be_valid
+  end
+
+  it 'should respond to name, phone, email' do #test that user has these dot methods
+    @user.should respond_to(:name)
+    @user.should respond_to(:phone)
+    @user.should respond_to(:email)
+  end
+
+  it 'should have a valid email' do #test email regex validation in config/initializers/devise.rb
+    emails = %w[john@smith,com john_at_smith.org example.john@smith.]
+    emails.each do |email|
+      user = User.create(name: "John Smith", email: email, password: "1234567890", password_confirmation: "1234567890")
+      user.should_not be_valid
+    end
+  end
+
+  it 'should have a unique email' do #test devise uniquess validation
+    user2 = User.create(name: "Jane Smith", email: "john@smith.com", password: "1234567890", password_confirmation: "1234567890")
+    user2.should_not be_valid
+  end
+
+  it 'should have a name' do #test user has name 
+    user3 = User.create(email: "john3@smith.com", password: "1234567890", password_confirmation: "1234567890")
+    user3.should_not be_valid
+  end
+
+  
+
 end
