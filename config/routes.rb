e854e5ba7e66e9348a17fdbb80656a083bbb9d1c
Dante Carlesimo <dantecarlesimo@gmail.com>
2014-05-27 21:32:31 -0700

    fixes for rspec

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Garage::Application.routes.draw do
   devise_for :users
   resources :sales
 
+
   post '/sales/:sale_id/items', to: "items#create"
   delete '/sales/:sale_id/items/:id', to: "items#destroy"
 end
